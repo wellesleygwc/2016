47bd5088b676813e3895defad9a58e38254dfea6
Jim Klumpp <jklumpp@tibco.com>
2017-02-10 10:30:03 -0500

    Initial checkin of core app

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+from flask import Flask, render_template, redirect, url_for, request, session
+import os, sys
+from app import db
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if db.check_password(username, password):
+            session['username'] = username
+            return redirect(url_for('home'))
+        else:
+            error = 'Invalid Credentials. Please try again.'
+            
+    return render_template('login.html', error=error)
+
+@app.route('/')
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    error = None
+    if 'username' in session:
+        return render_template('index.html', username=session['username'])
+    return redirect(url_for('login'))
+
+if __name__== "__main__":
+    db.create_db()
+    app.secret_key = os.urandom(24)
+    app.run(debug=True)
+    
