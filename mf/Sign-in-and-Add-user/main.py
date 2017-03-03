d210575f5644f615d124d376dee6c522f112c2a7
Jim Klumpp <jklumpp@tibco.com>
2017-03-03 08:59:24 -0500

    Meghan's project to sign in and add a user

---
+++ mf/Sign-in-and-Add-user/main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+# Imports
+from flask import Flask, render_template, redirect, url_for, request, session
+import os, sys
+from app import db
+
+app = Flask(__name__)
+
+# Login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if db.check_password(username, password):
+            session['username'] = username
+            return redirect(url_for('home'))
+        else:
+            session.clear()
+            error = 'Invalid Credentials. Please try again.'
+
+    return render_template('login.html', error=error)
+
+
+@app.route('/add user', methods=['GET', 'POST'])
+def addUser():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if db.create_user(username, password):
+            session['username'] = username
+            return redirect(url_for('home'))
+        else:
+            session.clear()
+            error = 'Invalid Credentials. Please try again.'
+
+    return render_template('login.html', error=error)
+
+
+# Home page
+@app.route('/')
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    error = None
+    if 'username' in session:
+        return render_template('index.html', username=session['username'])
+    return redirect(url_for('login'))
+
+if __name__== "__main__":
+    db.create_db()
+    app.secret_key = os.urandom(24)
+    app.run(debug=True)
---
+++ mf/Sign-in-and-Add-user/static/README.md
@@ -0,0 +1,2 @@
+# static
+This folder contains the database file that holds the users and questions
