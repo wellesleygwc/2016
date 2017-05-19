972e874fdd6dd9142cb446b3cc5326f9f6ecdc3c
sophiadigi <sophiadigi@gmail.com>
2017-05-18 20:14:07 -0400

    Version 101
    Added new page that states answer correctness of answered question

--- question-of-the-day/main.py
+++ question-of-the-day/main.py
@@ -59,6 +59,16 @@ def question():
     question = db.get_question(id)
     return render_template('Question.html', question=question, answers=db.get_question_answers(id))
 
+#Answers
+@app.route('/answers', methods=['GET', 'POST'])
+def answers():
+    correct = request.form['answer']
+    id = 0
+    if correct == 'w':
+        id = str('Your answer is wrong.')
+    elif correct == 'r':
+        id = str('Your answer is correct! Good job!')
+    return render_template('answers.html', id=id)
 
 # Login page
 @app.route('/')
